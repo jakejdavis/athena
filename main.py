ccee5a42a608a69c49f6d93ba01e872508b27493
Jake J Davis <jakejdavis1@gmail.com>
2023-03-17 15:02:59 +0000

    Return and save patched model

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import click
 from logger import logger, set_logger_level
 import importlib
 import os
-import h5py
 from typing import cast
 
 import operators.arachne_operators
@@ -12,41 +11,66 @@ import utils
 
 TRAINED_MODELS_DIR = "trained_models"
 
+
 class BasicCommand(click.Command):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.params.append(click.Option(['-v', '--verbose'], is_flag=True, help='Enable verbose output'))
+        self.params.append(
+            click.Option(
+                ["-v", "--verbose"], is_flag=True, help="Enable verbose output"
+            )
+        )
+
 
 @click.group()
 def cli():
-    pass 
+    pass
+
 
 @cli.command(cls=BasicCommand)
 @click.argument("subject_name")
 @click.argument("test_set")
-@click.option("-m", "--mutants_dir", default="mutants", help="Directory to load/save mutated models")
-@click.option("-r", "--metrics_dir", default="metrics", help="Directory to save the metrics of the mutation tests")
+@click.option(
+    "-m",
+    "--mutants_dir",
+    default="mutants",
+    help="Directory to load/save mutated models",
+)
+@click.option(
+    "-r",
+    "--metrics_dir",
+    default="metrics",
+    help="Directory to save the metrics of the mutation tests",
+)
 def run(subject_name, test_set, mutants_dir, metrics_dir, verbose):
     set_logger_level(verbose)
-    logger.info("Running mutation testing on subject %s with test set %s", subject_name, test_set)
+    logger.info(
+        "Running mutation testing on subject %s with test set %s",
+        subject_name,
+        test_set,
+    )
     logger.info("Mutants directory: %s", mutants_dir)
     logger.info("Metrics directory: %s", metrics_dir)
 
-    if not os.path.exists(os.path.join(TRAINED_MODELS_DIR, subject_name + "_trained.h5")):
-        logger.info(f"Trained model for subject {subject_name} not found. Training now...")
+    if not os.path.exists(
+        os.path.join(TRAINED_MODELS_DIR, subject_name + "_trained.h5")
+    ):
+        logger.info(
+            f"Trained model for subject {subject_name} not found. Training now..."
+        )
         subject_train = importlib.import_module(f"test_models.{subject_name}_train")
         subject_train.main(subject_name)
-
-    #if not os.path.exists(os.path.join(TRAINED_MODELS_DIR, subject_name + "_weak.h5")):
-    #    logger.info(f"Weakly trained model for subject {subject_name} not found. Training now...")
-    #    subject_train = importlib.import_module(f"test_models.{subject_name}_weak")
-    #    subject_train.main(subject_name)
-
     logger.info("Loading trained model")
-    model = cast(keras.Sequential, keras.models.load_model(os.path.join(TRAINED_MODELS_DIR, subject_name + "_trained.h5")))
-    i_pos, i_neg = utils.generate_inputs(subject_name, model)    
+    model = cast(
+        keras.Sequential,
+        keras.models.load_model(
+            os.path.join(TRAINED_MODELS_DIR, subject_name + "_trained.h5")
+        ),
+    )
+    pos, neg = utils.generate_inputs_outputs(subject_name, model)
 
-    operators.arachne_operators.operator_arachne(model, i_pos, i_neg)
+    patched_model = operators.arachne_operators.operator_arachne(model, pos, neg)
+    patched_model.save(os.path.join(TRAINED_MODELS_DIR, subject_name + "_patched.h5"))
 
 
 @cli.command(cls=BasicCommand)
@@ -57,14 +81,23 @@ def generate(model_file, mutants_dir, verbose):
     logger.info("Generating mutants for model %s", model_file)
     logger.info("Mutants directory: %s", mutants_dir)
 
+
 @cli.command(cls=BasicCommand)
 @click.argument("approach1_mutants_dir")
 @click.argument("approach2_mutants_dir")
-@click.option("-r", "--metrics_dir", default="metrics", help="Directory to save the metrics of the mutation tests")
+@click.option(
+    "-r",
+    "--metrics_dir",
+    default="metrics",
+    help="Directory to save the metrics of the mutation tests",
+)
 def compare(approach1_mutants_dir, approach2_mutants_dir, metrics_dir, verbose):
     set_logger_level(verbose)
-    logger.info("Comparing mutants from %s and %s", approach1_mutants_dir, approach2_mutants_dir)
+    logger.info(
+        "Comparing mutants from %s and %s", approach1_mutants_dir, approach2_mutants_dir
+    )
     logger.info("Metrics directory: %s", metrics_dir)
+
 
 if __name__ == "__main__":
     cli()
--- operators/arachne_operators.py
+++ operators/arachne_operators.py
@@ -1,11 +1,20 @@
-from .arachne import localisation 
+from .arachne import localisation
+from .arachne import searchers
 from keras.engine.sequential import Sequential
 from numpy import ndarray
+import logging
 
-def operator_arachne(model: Sequential, i_pos: ndarray = [], i_neg: ndarray = []) -> None:
+
+def operator_arachne(model: Sequential, pos: tuple, neg: tuple) -> None:
     assert model is not None, "Model not found"
-    assert len(i_pos) > 0, "No positive examples"
-    assert len(i_neg) > 0, "No negative examples"
-    assert len(i_neg) >= len(i_pos), "More positive examples than negative examples"
+    assert len(pos[0]) > 0, "No positive examples"
+    assert len(neg[0]) > 0, "No negative examples"
+    assert len(neg[0]) >= len(pos[0]), "More positive examples than negative examples"
 
-    localisation.bidirectional_localisation(model, i_pos, i_neg)
+    pareto, _ = localisation.bidirectional_localisation(model, pos, neg)
+    patch_searcher = searchers.DE(model, (pos, neg), pareto)
+    patch = patch_searcher.search().x
+    patched_model = patch_searcher.apply_patch(patch)
+    logging.info("Patch found: %s" % patch)
+
+    return patched_model
