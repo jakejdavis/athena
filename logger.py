a1ce3c6fa473abdf7a3f32375b10d519117f0395
Jake J Davis <jakejdavis1@gmail.com>
2023-03-20 13:44:30 +0000

    Ignore other loggers

--- logger.py
+++ logger.py
@@ -1,15 +1,31 @@
-import colorlog
+import io
 import logging
 
-handler = colorlog.StreamHandler()
+import colorlog
+import tensorflow as tf
 
-fmt = '[%(log_color)s%(levelname)s%(reset)s][%(name)s] %(message)s'
+EXTERNAL_LOGGERS = ["tensorflow-metal", "matplotlib", "h5py", "h5py_.conv"]
+
+handler = colorlog.StreamHandler()
+fmt = "[%(log_color)s%(levelname)s%(reset)s][%(name)s][%(filename)s] %(message)s"
 colorlog.basicConfig(level=logging.DEBUG, format=fmt)
-logger = colorlog.getLogger(__name__)
+
 
 def set_logger_level(verbose: bool) -> None:
+    logger = logging.getLogger()
+    logger.name = "athena"
     if verbose:
         logger.setLevel(logging.DEBUG)
         logger.debug("Debugging enabled")
     else:
         logger.setLevel(logging.INFO)
+
+    disable_external_loggers()
+
+
+def disable_external_loggers():
+    tf.get_logger().setLevel(logging.WARNING)
+
+    for external_logger in EXTERNAL_LOGGERS:
+        external_logger = logging.getLogger(external_logger)
+        external_logger.setLevel(logging.WARNING)
