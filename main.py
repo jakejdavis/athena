Fix patching and remove `variance`

main.py
@@ -3,6 +3,7 @@ import logging
 import os
 import pickle
 import sys
+import time
 
 import click
 import keras
@@ -132,6 +133,7 @@ def _generate(
 
     operator = operators.get_operator(operator_name)(model, additional_config)
 
+    time_start = time.time()
     logging.info(f"Generating mutant using {operator_name} operator")
     patched_model = operator(
         pos,
@@ -139,6 +141,7 @@ def _generate(
         pos_trivial=pos_trivial,
         neg_trivial=neg_trivial,
     )
+    time_to_generate = time.time() - time_start
 
     # Save mutant
 
@@ -154,7 +157,7 @@ def _generate(
         logging.info(f"Saving mutant to {patched_model_path}")
         patched_model.save(patched_model_path)
 
-    return model, patched_model
+    return model, patched_model, time_to_generate
 
 
 @cli.command(cls=BasicCommand)
@@ -213,7 +216,7 @@ def run(
         patched_model = keras.models.load_model(patched_model_path)
     else:
         logging.info("Generating patched model")
-        model, patched_model = _generate(
+        model, patched_model, _ = _generate(
             subject_name,
             trained_models_dir,
             mutants_dir,
@@ -353,31 +356,27 @@ def evaluate(
         additional_config, "evaluate.iterations", 10, int
     )
 
-    original_effect_sizes = []
-    patched_effect_sizes = []
+    original_model_accuracy = []
+    patched_model_accuracy = []
+
+    original_model_accuracy_trivial = []
+    patched_model_accuracy_trivial = []
+
+    times_to_generate = []
 
     for i in tqdm(range(iterations)):
         logging.info(f"Running iteration {i+1}/{iterations}")
 
         model_utils = models.get_model(subject_name)(additional_config)
 
-        if True:
-            model, patched_model = _generate(
-                subject_name,
-                trained_models_dir,
-                mutants_dir,
-                specific_output,
-                additional_config,
-            )
-        else:
-            import tensorflow as tf
-
-            model = tf.keras.models.load_model(
-                f"trained_models/{subject_name}_trained.h5"
-            )
-            patched_model = tf.keras.models.load_model(
-                f"mutants/{subject_name}_patched_0.h5"
-            )
+        model, patched_model, time_to_generate = _generate(
+            subject_name,
+            trained_models_dir,
+            mutants_dir,
+            specific_output,
+            additional_config,
+        )
+        times_to_generate.append(time_to_generate)
 
         # evaluate model and patched_model and calculate effect size
         specific_output_int = None
@@ -388,37 +387,65 @@ def evaluate(
             specific_output=specific_output_int, trivial=False
         )
 
-        original_effect_sizes.append(model.evaluate(inputs, outputs, verbose=0)[1])
-        patched_effect_sizes.append(
+        original_model_accuracy.append(model.evaluate(inputs, outputs, verbose=0)[1])
+        patched_model_accuracy.append(
             patched_model.evaluate(inputs, outputs, verbose=0)[1]
         )
 
-        logging.info("Iteration complete!")
-        logging.info(f"- Original model accuracy: {original_effect_sizes[-1]}")
-        logging.info(f"- Patched model accuracy: {patched_effect_sizes[-1]}")
-
-    original_effect_sizes = np.array(original_effect_sizes)
-    patched_effect_sizes = np.array(patched_effect_sizes)
-
-    is_significant, p_value, effect_size = utils.stats.is_significant(
-        original_effect_sizes, patched_effect_sizes
-    )
-
-    if is_significant:
-        logging.info(
-            f"\u2713 The effect size of the mutation is significant (p-value = {p_value}, effect size = {effect_size})"
+        trivial_inputs, trivial_outputs = model_utils.generate_evaluation_data(
+            specific_output=specific_output_int, trivial=True
         )
-    else:
-        logging.info(
-            f"\u2717 The effect size of the mutation is not significant (p-value = {p_value}, effect size = {effect_size})"
+
+        original_model_accuracy_trivial.append(
+            model.evaluate(trivial_inputs, trivial_outputs, verbose=0)[1]
         )
+        patched_model_accuracy_trivial.append(
+            patched_model.evaluate(trivial_inputs, trivial_outputs, verbose=0)[1]
+        )
+
+        logging.info(f"Iteration complete (took {times_to_generate[-1]} seconds)")
+        logging.info(f"- Original model accuracy: {original_model_accuracy[-1]}")
+        logging.info(f"- Patched model accuracy: {patched_model_accuracy[-1]}")
+        logging.info(
+            f"- Original model accuracy (trivial): {original_model_accuracy_trivial[-1]}"
+        )
+        logging.info(
+            f"- Patched model accuracy (trivial): {patched_model_accuracy_trivial[-1]}"
+        )
+
+    original_model_accuracy = np.array(original_model_accuracy)
+    patched_model_accuracy = np.array(patched_model_accuracy)
+
+    original_model_accuracy_trivial = np.array(original_model_accuracy_trivial)
+    patched_model_accuracy_trivial = np.array(patched_model_accuracy_trivial)
+
+    try:
+        is_significant, p_value, effect_size = utils.stats.is_significant(
+            original_model_accuracy, patched_model_accuracy
+        )
+
+        if is_significant:
+            logging.info(
+                f"\u2713 The effect size of the mutation is significant (p-value = {p_value}, effect size = {effect_size})"
+            )
+        else:
+            logging.info(
+                f"\u2717 The effect size of the mutation is not significant (p-value = {p_value}, effect size = {effect_size})"
+            )
+    except Exception as e:
+        logging.error(f"Could not calculate effect size: {e}")
+        p_value = None
+        effect_size = None
 
     # Save evaluation to file
-    with open(f"evaluation/{subject_name}_evaluation.json", "w") as f:
+    with open(f"evaluation/{subject_name}_evaluation_{specific_output}.json", "w") as f:
         json.dump(
             {
-                "original_effect_sizes": original_effect_sizes.tolist(),
-                "patched_effect_sizes": patched_effect_sizes.tolist(),
+                "original_model_accuracy": original_model_accuracy.tolist(),
+                "patched_model_accuracy": patched_model_accuracy.tolist(),
+                "original_model_accuracy_trivial": original_model_accuracy_trivial.tolist(),
+                "patched_model_accuracy_trivial": patched_model_accuracy_trivial.tolist(),
+                "time_to_generate": times_to_generate,
                 "p_value": p_value,
                 "effect_size": effect_size,
             },

operators/athena/searchers/de.py
@@ -3,6 +3,7 @@ import os
 from multiprocessing import Queue
 from typing import List, Tuple, Union
 
+import keras.losses
 import numpy as np
 import tensorflow as tf
 from keras.engine.sequential import Sequential
@@ -18,15 +19,12 @@ from .searcher import Searcher
 Nfeval = 1
 
 
-def score(
-    model: Sequential, inputs_outputs: Tuple[ndarray, ndarray], variance=None
-) -> float:
+def score(model: Sequential, inputs_outputs: Tuple[ndarray, ndarray]) -> float:
     """
     Score for model on inputs and outputs.
 
     :param model: Model to score
     :param inputs_outputs: Inputs and outputs to score on
-    :param variance: Used in regression, variance of a correct prediction
     """
     inputs, outputs = inputs_outputs
 
@@ -35,9 +33,18 @@ def score(
     _score = 0.0
 
     for i, prediction in enumerate(predictions):
-        prediction_correct = (
-            is_classification(model) and np.argmax(prediction) == np.argmax(outputs[i])
-        ) or (not is_classification(model) and abs(prediction - outputs[i]) < variance)
+        prediction_correct = False
+        if model.loss == keras.losses.categorical_crossentropy:
+            prediction_correct = np.argmax(prediction) == np.argmax(outputs[i])
+        elif model.loss == keras.losses.binary_crossentropy:
+            prediction_correct = (
+                prediction > 0.5
+                and outputs[i] == 1
+                or prediction < 0.5
+                and outputs[i] == 0
+            )
+        else:
+            logging.error(f"Unknown loss function: {model.loss}")
 
         if prediction_correct:
             _score += 1.0
@@ -51,7 +58,6 @@ def fitness_score(
     patched_model: Sequential,
     pos,
     neg,
-    variance: float,
     alpha: float,
 ) -> float:
     """
@@ -60,11 +66,10 @@ def fitness_score(
     :param patched_model: Model to score
     :param pos: Positive inputs and outputs
     :param neg: Negative inputs and outputs
-    :param variance: Used in regression, variance of a correct prediction
     :param alpha: Weighting of negative fitness
     """
-    neg_fitness = score(patched_model, (neg[0], neg[1]), variance)
-    pos_fitness = score(patched_model, (pos[0], pos[1]), variance)
+    neg_fitness = score(patched_model, (neg[0], neg[1]))
+    pos_fitness = score(patched_model, (pos[0], pos[1]))
 
     return pos_fitness + alpha * neg_fitness
 
@@ -79,7 +84,6 @@ def fitness(
     weights_to_target: List[Tuple[int64, Tuple[int64, int64]]],
     trivial_weighting: float,
     alpha: float,
-    variance: float,
 ) -> ndarray:
     """
     Fitness function for differential evolution.
@@ -93,7 +97,6 @@ def fitness(
     :param weights_to_target: Weights to target
     :param trivial_weighting: Weighting of trivial fitness
     :param alpha: Weighting of negative fitness
-    :param variance: Used in regression, variance of a correct prediction
     """
 
     if isinstance(model, str):
@@ -111,25 +114,25 @@ def fitness(
         logging.debug("No weights to apply")
         new_model = model
 
-    total_fitness = fitness_score(new_model, pos, neg, variance, alpha)
+    total_fitness = fitness_score(new_model, pos, neg, alpha)
 
     # Maxmimise fitness of outputs not being targetted
     if pos_trivial is not None:
-        total_fitness += (
-            -1
-            * trivial_weighting
-            * fitness_score(new_model, pos_trivial, neg_trivial, variance, alpha)
+        trivial_fitness = -fitness_score(new_model, neg_trivial, pos_trivial, alpha)
+        total_fitness = (
+            trivial_weighting * trivial_fitness
+            + (1 - trivial_weighting) * total_fitness
         )
 
     return total_fitness
 
 
 def apply_patch(model, patched_weights: ndarray, weights_to_target) -> Sequential:
-    for i, (layer_index, neuron) in enumerate(weights_to_target):
+    for i, (layer_index, weight_indexes) in enumerate(weights_to_target):
         model.layers[layer_index].weights[0].assign(
             tf.tensor_scatter_nd_update(
                 model.layers[layer_index].weights[0],
-                [neuron],
+                [weight_indexes],
                 [patched_weights[i]],
             )
         )
@@ -165,33 +168,13 @@ class DE(Searcher):
         self.alpha = get_config_val(
             self.additional_config, "operator.searcher.fitness.alpha", 0.8, float
         )
-        self.variance = get_config_val(
-            config=self.additional_config, key="correct_variance", default=1, type=float
-        )
 
         self.trivial_weighting = get_config_val(
             self.additional_config,
             "operator.searcher.fitness.trivial_weighting",
-            None,
+            0.5,
             float,
         )
-
-        if self.trivial_weighting is None:
-            logging.info("Weighting trivial fitness for initial fitness ~= 0")
-            standard_fitness_score = fitness_score(
-                self.model, self.pos, self.neg, self.variance, self.alpha
-            )
-
-            trivial_fitness_score = fitness_score(
-                self.model,
-                self.trivial_pos,
-                self.trivial_neg,
-                self.variance,
-                self.alpha,
-            )
-
-            self.trivial_weighting = standard_fitness_score / trivial_fitness_score
-            logging.info(f"Trivial weighting: {self.trivial_weighting}")
 
         self.plot = get_config_val(
             self.additional_config, "operator.searcher.plot.show", False, bool
@@ -220,7 +203,6 @@ class DE(Searcher):
                     self.weights_to_target,
                     self.trivial_weighting,
                     self.alpha,
-                    self.variance,
                 )
 
                 logging.info(f"Starting with fitness {initial_fitness}")
@@ -251,7 +233,6 @@ class DE(Searcher):
             self.weights_to_target,
             self.trivial_weighting,
             self.alpha,
-            self.variance,
         )
         logger = logging.getLogger("athena")
 
@@ -295,6 +276,13 @@ class DE(Searcher):
                 os.makedirs(os.path.dirname(self.TEMP_MODEL_PATH), exist_ok=True)
             self.model.save(self.TEMP_MODEL_PATH)
 
+        initial_weights = np.array(
+            [
+                (self.model.layers[layer].get_weights()[0][index])
+                for layer, (index) in self.weights_to_target
+            ]
+        )
+
         return differential_evolution(
             fitness,
             bounds,
@@ -310,6 +298,7 @@ class DE(Searcher):
             mutation=(0.25, 1.5),
             recombination=0.7,
             init="latinhypercube",
+            x0=initial_weights,
             workers=self.workers,
             updating="deferred",
             callback=self.callback_print,
@@ -322,6 +311,5 @@ class DE(Searcher):
                 self.weights_to_target,
                 self.trivial_weighting,
                 self.alpha,
-                self.variance,
             ),
         )